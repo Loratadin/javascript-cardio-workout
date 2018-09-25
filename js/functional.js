98de9d90f27368c93dc06bd25f48b497ed46f20f
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-09-25 11:15:16 -0500

    Add variables and reassign the value

--- js/functional.js
+++ js/functional.js
@@ -128,3 +128,11 @@ console.table(yearsLived);
           }
           console.log(pressed);
       })
+      //Reference
+      let age = 100; 
+      let age2 = age;
+      console.log(age, age2);
+      age = 200;
+      console.log(age, age2);
+
+      
