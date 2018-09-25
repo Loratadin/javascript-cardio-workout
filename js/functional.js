3b3f54a94edee5b5946cb1dbe38f9420581fbd2d
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-09-25 14:56:23 -0500

    Write palindrome function

--- js/functional.js
+++ js/functional.js
@@ -135,4 +135,12 @@ console.table(yearsLived);
       age = 200;
       console.log(age, age2);
 
-      
+      //Palindrome
+      function palindrome(str) {
+          let reverseWord = "";
+          for (let i = str.length -1; i >= 0; i--){
+              reverseWord += str[i];
+          }
+          return reverseWord === str;
+      }
+      console.log(madam);
