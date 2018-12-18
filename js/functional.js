ed9291f421d51b6dd56cd3b74a57ed3088008ecc
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-12-18 01:53:01 -0600

    add rest operator

--- js/functional.js
+++ js/functional.js
@@ -274,3 +274,14 @@ function isEqualTo(number, compare) {
     return number == compare;
 }
 console.log(isEqualTo(10, 10));
+
+// Rest Operator
+function sumUp(...toAdd) {
+    console.log(toAdd);
+    let result = 0;
+    for (let i = 0; i < toAdd.length; i++) {
+        result += toAdd[i];
+    }
+    return result;
+}
+console.log(sumUp(100, 20, 10));
