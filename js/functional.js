fceb74b2950d06570dd753048e96390af173c20c
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-01-08 09:06:17 -0600

    chain promises

--- js/functional.js
+++ js/functional.js
@@ -382,3 +382,19 @@ let promise = new Promise(function (resolve, reject) {
 promise.then(function (value) {
     console.log(value);
 });
+
+// chain promises
+function waitASecond(seconds) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            seconds++;
+            resolve(seconds);
+        }, 1000);
+    });
+}
+
+waitASecond(0)
+    .then(waitASecond)
+    .then(function (seconds) {
+        console.log(seconds);
+    })
