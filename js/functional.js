0cba1b187e51e3a78ae7398a5a295fe5daf534da
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-01-16 15:55:48 -0600

    manage items in a set

--- js/functional.js
+++ js/functional.js
@@ -454,3 +454,12 @@ let deck = new Map();
 deck.set("as", cardAce);
 deck.set('kc', cardKing);
 
+// Set
+let set = new Set([1, 1, 1]);
+ 
+set.add(2);
+set.delete(1);
+
+for (element of set) {
+    console.log(element);
+}
