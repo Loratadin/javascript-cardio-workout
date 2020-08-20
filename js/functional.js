42e028b23dce24abb07f01969764c13f45e47129
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-08-19 22:16:14 -0500

    add weave function

--- js/functional.js
+++ js/functional.js
@@ -736,4 +736,21 @@ class Queue {
     remove() {
         return this.data.pop();
     }
+    peek() {
+        return this.data[this.data.length - 1];
+    }
 }
+
+function weave(sourceOne, sourceTwo) {
+    const q = new Queue();
+    while (sourceOne.peek() || sourceTwo.peek()) {
+        if (sourceOne.peek()) {
+            q.add(sourceOne.remove());
+        }
+        if (sourceTwo.peek()) {
+            q.add(sourceTwo.remove());
+        }
+    }
+    return q;
+}
+console.log(weave([1, 2, 3], ["Hi", "There", "You"]));
