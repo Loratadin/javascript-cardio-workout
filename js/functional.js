5a4a1428e7c25259a6c02da824afcc9c33227eb0
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-08-31 15:02:06 -0500

    find a midpoint in a linked list

--- js/functional.js
+++ js/functional.js
@@ -929,4 +929,12 @@ class LinkedList  {
         previous.next = node;
     }
 }
-
+function midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+}
