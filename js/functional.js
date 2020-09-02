ed8825f7663b95f7b0b38662a18977b1f5fc946d
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-09-02 15:49:34 -0500

    find a node n steps from the last node in a linked list

--- js/functional.js
+++ js/functional.js
@@ -949,3 +949,16 @@ function loopThrough(list) {
         }
     }
 }
+function fromLast(list, n) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+    while (n > 0) {
+        fast = fast.next;
+        n--;
+    }
+    while (fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return slow;
+}
