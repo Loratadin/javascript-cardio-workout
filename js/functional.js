1db1ba3322f43fa62573ce48e712037363eb7a11
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-09-02 15:34:34 -0500

    loop through a linked list

--- js/functional.js
+++ js/functional.js
@@ -938,3 +938,14 @@ function midpoint(list) {
     }
     return slow;
 }
+function loopThrough(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+}
