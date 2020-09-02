26bb55f7a91e5e8fe496a06f5d10eb98f284e527
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-09-02 16:13:33 -0500

    implement a node for a tree

--- js/functional.js
+++ js/functional.js
@@ -962,3 +962,18 @@ function fromLast(list, n) {
     }
     return slow;
 }
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
+    add(data) {
+        const node = new Node(data);
+        this.children.push(node);
+    }
+    remove(data) {
+        this.children.filter(node => {
+            return node.data !== data;
+        })
+    }
+}
