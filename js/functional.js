b8ebeef07ec5ddbe8d3d94c023fba4ef1dc0f2f8
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-09-06 21:53:00 -0500

    insert nodes in binary search tree

--- js/functional.js
+++ js/functional.js
@@ -1015,3 +1015,23 @@ function levelWidth(root) {
     }
     return counters;
 }
+
+// insert nodes in binary search tree
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    insert(data) {
+        if (data < this.data && this.left) {
+            this.left.insert(data);
+        } else if (data < this.data) {
+            this.left = new Node(data);
+        } else if (data > this.data && this.right) {
+            this.right.insert(data);
+        } else if (data > this.data) {
+            this.right = new Node(data);
+        }
+    }
+}
